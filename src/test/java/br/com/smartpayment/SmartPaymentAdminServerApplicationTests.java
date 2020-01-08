f84e1962c4b75afd8e6f32142a27fe183d26bf70
Gabriel Martins <ga.smartins94@gmail.com>
2020-01-07 22:16:42 -0300

    First Commit

---
+++ src/test/java/br/com/smartpayment/SmartPaymentAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPaymentAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
